4f07966d1219231cb0f6d3382540740c083e7b31
CJAX.Dev <jackson102890@gmail.com>
2025-04-29 10:40:01 -0600

    Fix Cart Delete Button Position/Size

---
+++ js/features.js
@@ -0,0 +1,17 @@
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const mainNav = document.getElementById('main-nav');
+const header = document.getElementsByTagName('header');
+hamburgerIcon.addEventListener('click', () => {
+    if (hamburgerIcon.ariaExpanded === 'false') {
+        hamburgerIcon.ariaExpanded = 'true';
+        hamburgerIcon.classList.add('show');
+        mainNav.classList.add('show');
+    }
+    
+    else if (hamburgerIcon.ariaExpanded === 'true') {
+        hamburgerIcon.ariaExpanded = 'false';
+        hamburgerIcon.classList.remove('show');
+        mainNav.classList.remove('show');
+    }
+    
+})
